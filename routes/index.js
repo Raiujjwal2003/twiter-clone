3dd0c4fd4559a5acc93637a31b32ea4427f284fb
Raiujjwal2003 <raiujjwal2003@gmail.com>
2023-09-10 20:26:04 +0530

    change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
+const userModel= require("./users")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
 
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign_up', { title: 'Express' });
+});
+
+router.post('/register_user',function(req, res){
+  userModel.create({
+    name:req.body.name,
+    username:req.body.username,
+    email:req.body.email,
+    password:req.body.password,
+
+
+  }).then(function(createduser){
+      res.send(createduser);
+  }) 
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,77 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://localhost/twiter_user")
+.then(function(){
+  console.log("database se conncet ho gayaa hui hui huii...");
+})
 
-module.exports = router;
+var userSchema = mongoose.Schema({
+  name:String,
+  username:String,
+  email:String,
+  password: String,
+ 
+})
+
+
+// // const mongoose = require("mongoose");
+
+// // // Replace <username>, <password>, and <database-name> with your MongoDB Atlas credentials
+// // const atlasConnectionUri = "mongodb+srv://raiujjwal2003:Ujjwal2003@Rai@cluster.mongodb.net/twitter_users";
+
+// // mongoose.connect(atlasConnectionUri, {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// // })
+// // .then(function(){
+// //   console.log("Connected to MongoDB Atlas...");
+// // })
+// // .catch(function(err) {
+// //   console.error("Error connecting to MongoDB Atlas:", err);
+// // });
+
+// // var userSchema = mongoose.Schema({
+// //   email: String,
+// //   password: String,
+// // });
+
+// module.exports = mongoose.model("users", userSchema);
+
+
+// // module.exports = mongoose.model("users", userSchema);
+
+
+
+// const mongoose = require("mongoose");
+
+// // Replace <username>, <password>, and <database-name> with your MongoDB Atlas credentials
+// const atlasConnectionUri =
+//   "mongodb+srv://raiujjwal2003:Ujjwal2003@Rai@cluster.mongodb.net/twitter_users";
+
+// // Connect to MongoDB Atlas
+// mongoose.connect(atlasConnectionUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// // Create a connection reference
+// const db = mongoose.connection;
+
+// // Handle connection errors
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// // Notify when the connection is successfully established
+// db.once("open", () => {
+//   console.log("Connected to MongoDB Atlas");
+// });
+
+// // Define your mongoose schema and model here if needed
+// const userSchema = mongoose.Schema({
+//   email: String,
+//   password: String,
+// });
+
+const UserModel = mongoose.model("User", userSchema);
+
+// Export the UserModel or other components as needed
+module.exports = UserModel;
